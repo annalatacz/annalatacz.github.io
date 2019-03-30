dcd0581e0bfb890b4dfa44b3988fda347efa3bad
annalatacz <ann.latacz@gmail.com>
2019-03-30 11:13:51 +0000

    Cookies
    Added banner/consent form and cookie scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,84 @@
-var renderer_ = i2d.SVGLayer('#Mycanvas', {
-    events: false,
-    selectiveClear: false
-});
-var parallelChain = i2d.chain.parallelChain().loop(10)
-var circlesCount = 100
-var radius = 50
-
-var g = renderer_.createEl({
-    el: 'group',
-    attr: {
-        transform: {
-            translate: [renderer_.width / 2, renderer_.height / 2]
-        }
-    }
-})
-
-g.createEls((new Array(circlesCount)).fill().map(function (d, i) {
-    return i
-}), {
-        el: 'circle',
-        attr: {
-            r: 5,
-            cx: 0,
-            cy: 0
-        },
-        style: {
-            fill: function (d) {
-                return 'hsl(' + ((d % 50) / 50) * 360 + ',70%, 50%)'
-            }
-        }
-    })
-    .exec(animateEachCircle)
-
-function animateEachCircle(d) {
-    parallelChain.add(this.animateExe({
-        duration: 2000,
-        delay: (d % 50) * 30,
-        ease: 'easeInOutSin',
-        attr: function (f) {
-            this.setAttr({
-                cx: radius * Math.cos(f * Math.PI * 2 + Math.PI * Math.floor(d / 50)) + (-radius + Math.floor(d / 50) * radius * 2),
-                cy: radius * Math.sin(f * Math.PI * 2 + Math.PI * Math.floor(d / 50))
-            })
-        }
-    }))
+﻿function checkCookiesPermissions() 
+{
+	if (navigator.cookieEnabled == true) 
+	{
+		alert("Cookies are enabled.");
+		checkCookie();
+	} 
+	else 
+	{
+		alert("Cookies are not enabled.");
+	}
 }
 
-parallelChain.start()
+function setCookie(cname)
+{
+	cname = "lastVisitDate";
+	var now = new Date();
+	var today = new Date();
+
+	var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()+ '-';
+	var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+	var dateTime = date + ' ' + time;
+
+	now.setMinutes(now.getMinutes() + 3);
+
+	var expires = "expires=" + now;
+	document.cookie = cname + "=" + dateTime + ";" + expires + ";path=/";
+}
+
+function getCookie(cname)
+{
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for (var i = 0; i < ca.length; i++)
+	{
+		var c = ca[i];
+		while (c.charAt(0) == ' ')
+		{
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0)
+		{
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+function checkCookie()
+{
+	var date = getCookie("lastVisitDate");
+
+	if (date == "")
+	{
+		alert("Hello new Visitor!");
+	}
+	else
+	{
+		var parts = date.split('-');
+
+		var year = Number(parts[0]);
+		var month = Number(parts[1] - 1);
+		var day = Number(parts[2]);
+
+		var today = new Date();
+		var day2 = Number(today.getDate());
+		var month2 = Number(today.getMonth());
+		var year2 = Number(today.getFullYear());
+
+		var count = ((year2 - year) * 12 + month2 - month)*30 + day2 - day; // The average day count for a month is 30 days to simplify calculations.
+
+		if (isNaN(count)) //“is Not a Number”, if variable is not a number, it return true, else return false.
+		{
+			alert("Hello new Visitor!"); //this may happen if there is something wrong with the date in the cookie - special case issue
+		}
+		else
+		{
+			alert("Welcome again! It's nice to see you after " + count + " days." ); //+ year +" "+ month +" "+ day+ " "+ year2 +" "+ month2 +" "+ day2+ " " +count);
+		}
+	}
+
+	setCookie("lastVisitDate");
+}
